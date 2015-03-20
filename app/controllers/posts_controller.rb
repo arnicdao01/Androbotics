b4f51cf252018471089e1aa4f8f5a185b4431f21
Arjay Nicdao <a.nicdao@androbo.jp>
2015-03-20 13:59:14 +0800

    #3 commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  def index
+    @content_one = 'This is just a sample post 1';
+    @content_two = 'This is the second post';
+  end
+
+  def new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+  def update
+
+  end
+
+  def show
+
+  end
+
+  def destroy
+
+  end
+end
